3b737189ed385b242e9b238fc9308d86b5f3edb4
Marek Rogalski <mafikpl@gmail.com>
2025-06-12 11:46:13 +0200

    Commit config.h ignoring .gitignore

---
+++ src/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <Arduino.h>
+
+constexpr bool kDebug = false;
+const char *kWiFiSSID PROGMEM = "your wifi name";
+const char *kWiFiPass PROGMEM = "your wifi password";
+const char *kTZ PROGMEM = "CET-1CEST,M3.5.0,M10.5.0/3"; // your timezone string
